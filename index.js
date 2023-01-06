0e426a17bd52899c375ab8e448bb8410e4d3bf76
isarohmadan <isagaul1z2@gmail.com>
2023-01-06 22:41:55 +0800

    10% web mini e-commerese

--- index.js
+++ index.js
@@ -1,71 +1,92 @@
 // mengambil argument dari command line
-const { describe } = require('yargs');
-const yargs = require('yargs');
-const { simpanContact,listContacts,detailContact,deleteContact } = require('./contact');
+// const { describe } = require('yargs');
+// const yargs = require('yargs');
+// const { simpanContact,listContacts,detailContact,deleteContact } = require('./contact');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.set(bodyParser.urlencoded({extended:true}))
+app.set('view engine','ejs')
+app.set('views','view')
+app.use(express.static(path.join(__dirname,'asset')))
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        title : "Halaman Home"
+    })
+})
 
 
-yargs.command({
-    command : 'add',
-    describe : 'Menambahkan contact baru',
-    builder : {
-        nama : {
-            describe : 'Nama Panjang',
-            demandOption : true,
-            type : 'string' 
-        },
-        no : {
-            describe : 'Nomor telepon',
-            demandOption : false ,
-            type : 'string'
-        },
-        email : {
-            describe : 'email',
-            demandOption : true,
-            type : 'string'
-        }
-    },
-    handler(argv){
-        simpanContact(argv.nama,argv.no,argv.email)
-    }
+app.listen('9000',(err)=>{
+    if(err)
+        throw err
+    console.log("koneksi berhasil di port 9000");
+
+})
+// yargs.command({
+//     command : 'add',
+//     describe : 'Menambahkan contact baru',
+//     builder : {
+//         nama : {
+//             describe : 'Nama Panjang',
+//             demandOption : true,
+//             type : 'string' 
+//         },
+//         no : {
+//             describe : 'Nomor telepon',
+//             demandOption : false ,
+//             type : 'string'
+//         },
+//         email : {
+//             describe : 'email',
+//             demandOption : true,
+//             type : 'string'
+//         }
+//     },
+//     handler(argv){
+//         simpanContact(argv.nama,argv.no,argv.email)
+//     }
     
-}).demandCommand()
+// }).demandCommand()
 
-yargs.command({
-    command : 'list',
-    describe : "menampilkan nama dan no hp",
-    handler(){
-        listContacts()
-    }
-})
+// yargs.command({
+//     command : 'list',
+//     describe : "menampilkan nama dan no hp",
+//     handler(){
+//         listContacts()
+//     }
+// })
 
-yargs.command({
-    command : 'detail',
-    describe : "menampilkan detail",
-    builder : {
-        nama : {
-            describe : 'Nama Panjang',
-            demandOption : true,
-            type : 'string' 
-        },
-    },
-    handler(argv){
-        detailContact(argv.nama)
-    }
-})
+// yargs.command({
+//     command : 'detail',
+//     describe : "menampilkan detail",
+//     builder : {
+//         nama : {
+//             describe : 'Nama Panjang',
+//             demandOption : true,
+//             type : 'string' 
+//         },
+//     },
+//     handler(argv){
+//         detailContact(argv.nama)
+//     }
+// })
 
-yargs.command({
-    command : 'delete',
-    describe : "menghapus kontak berdasarkan nama",
-    builder : {
-        nama : {
-            describe : 'Nama Panjang',
-            demandOption : true,
-            type : 'string' 
-        },
-    },
-    handler(argv){
-        deleteContact(argv.nama)
-    }
-})
+// yargs.command({
+//     command : 'delete',
+//     describe : "menghapus kontak berdasarkan nama",
+//     builder : {
+//         nama : {
+//             describe : 'Nama Panjang',
+//             demandOption : true,
+//             type : 'string' 
+//         },
+//     },
+//     handler(argv){
+//         deleteContact(argv.nama)
+//     }
+// })
 
-yargs.parse();
+// yargs.parse();
